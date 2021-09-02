91663ee787b72d487927a5f129f85eed2bd5bd44
= <srikanthburra860@gmail.com>
2021-09-02 13:49:10 +0530

    home screen UI part done

--- lib/navigation/Home.dart
+++ lib/navigation/Home.dart
@@ -10,6 +10,7 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
   List<bool> selected = [true, false, false, false];
+  TextEditingController searchCtrl = TextEditingController();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -194,8 +195,360 @@ class HomeState extends State<Home> {
                 color: Color(0xffeeeeef),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(8)))),
-          )
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 16, right: 16),
+            height: 32,
+            child: TextField(
+              controller: searchCtrl,
+              decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Color(0xff838387),
+                  ),
+                  border: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  isDense: true,
+                  enabledBorder: InputBorder.none,
+                  hintText: "Search Vehicle Number",
+                  hintStyle: TextStyle(fontSize: 16, color: Color(0xff838387))),
+            ),
+            decoration: ShapeDecoration(
+                color: Color(0xffeeeeef),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)))),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Flexible(
+              child: ListView(
+            children: [for (int i = 0; i < 32; i++) builder()],
+          ))
         ],
+      ),
+    );
+  }
+
+  Widget builder() {
+    return Container(
+      margin: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.only(top: 16),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Icon(
+                      Icons.directions_bus,
+                      color: Colors.blue,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "AEW4630",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "0.0 km/h",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black),
+                        ),
+                      ],
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                    ),
+                    Spacer(),
+                    Column(
+                      children: [
+                        Text(
+                          "25-08-21 15:44:03",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "STOP",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.red),
+                        ),
+                      ],
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                    ),
+                    SizedBox(
+                      width: 16,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                        child: Container(
+                      child: Text(
+                        "2-4-28 Level 4, Harbour Trade Centre, Gat Lebuh Macallum, 10300 George Town, Penang, Malaysia",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    )),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Icon(
+                      Icons.share,
+                      color: Color(0xff4b5fc3),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  height: 2,
+                  width: double.infinity,
+                  color: Color(0xffc6c6c8),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.add_road,
+                              color: Colors.green,
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              "2181.5 Km",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          "Distance",
+                          style: TextStyle(
+                              color: Color(0xff939397),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
+                    Spacer(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.local_gas_station,
+                              color: Colors.red,
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              "0.0 L",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          "Fuel Volume",
+                          style: TextStyle(
+                              color: Color(0xff939397),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
+                    Spacer(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.power,
+                              color: Colors.green,
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              "3.6 V",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          "Truck Voltage",
+                          style: TextStyle(
+                              color: Color(0xff939397),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
+                    Spacer(),
+                  ],
+                ),
+                SizedBox(
+                  height: 4,
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Color(0xffc6c6c8), width: 1),
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+            ),
+          ),
+          Container(
+              height: 36,
+              child: Row(
+                children: [
+                  SizedBox(width: 16),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(24)),
+                      color: Colors.white,
+                    ),
+                    child: Icon(
+                      Icons.wifi,
+                      color: Colors.green,
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(24)),
+                      color: Colors.white,
+                    ),
+                    child: Icon(
+                      Icons.cable,
+                      color: Colors.red,
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(24)),
+                      color: Colors.white,
+                    ),
+                    child: Icon(
+                      Icons.podcasts,
+                      color: Colors.green,
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    "PTO",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Spacer(),
+                  Text(
+                    "SOS",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Spacer(),
+                  Text(
+                    "IMMO",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(width: 16),
+                ],
+              ))
+        ],
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+      decoration: BoxDecoration(
+        color: Color(0xff36bab2),
+        border: Border.all(color: Color(0xffc6c6c8), width: 1),
+        borderRadius: BorderRadius.all(Radius.circular(16)),
       ),
     );
   }
--- lib/navigation/tab_bar.dart
+++ lib/navigation/tab_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gps/models/user.dart';
-import 'package:gps/navigation/DashBoard.dart';
 import 'package:gps/navigation/Home.dart';
 import 'package:gps/navigation/Map.dart';
 import 'package:gps/navigation/Report.dart';
@@ -19,18 +18,17 @@ class tabBar extends StatefulWidget {
 class tabBarState extends State<tabBar> {
   User user;
   tabBarState(this.user);
-  List<bool> selected = [false, true, false, false, false];
+  List<bool> selected = [true, false, false, false];
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return DefaultTabController(
-        length: 5,
-        initialIndex: 1,
+        length: 4,
+        initialIndex: 0,
         child: Scaffold(
           body: TabBarView(
             physics: NeverScrollableScrollPhysics(),
             children: [
-              DashBoard(),
               Home(),
               MapScreen(),
               Report(),
@@ -39,7 +37,7 @@ class tabBarState extends State<tabBar> {
           ),
           bottomNavigationBar: TabBar(
             onTap: (index) {
-              for (int i = 0; i < 5; i++) {
+              for (int i = 0; i < 4; i++) {
                 if (i != index) {
                   setState(() {
                     selected[i] = false;
@@ -53,43 +51,36 @@ class tabBarState extends State<tabBar> {
             },
             indicatorColor: Colors.transparent,
             unselectedLabelStyle: TextStyle(color: Colors.grey, fontSize: 8),
-            labelStyle: TextStyle(color: Colors.blue, fontSize: 9),
+            labelStyle: TextStyle(color: Colors.blue, fontSize: 10),
             labelColor: Colors.blue,
             unselectedLabelColor: Colors.grey,
             tabs: [
               Tab(
-                text: "Dash board",
-                icon: Icon(
-                  Icons.dashboard,
-                  color: selected[0] ? Colors.green : Colors.grey,
-                ),
-              ),
-              Tab(
                 text: "Home",
                 icon: Icon(
                   Icons.home,
-                  color: selected[1] ? Colors.green : Colors.grey,
+                  color: selected[0] ? Colors.green : Colors.grey,
                 ),
               ),
               Tab(
                 text: "Map",
                 icon: Icon(
                   Icons.map,
-                  color: selected[2] ? Colors.green : Colors.grey,
+                  color: selected[1] ? Colors.green : Colors.grey,
                 ),
               ),
               Tab(
                 text: "Reports",
                 icon: Icon(
-                  Icons.article,
-                  color: selected[3] ? Colors.green : Colors.grey,
+                  Icons.local_library_outlined,
+                  color: selected[2] ? Colors.green : Colors.grey,
                 ),
               ),
               Tab(
                 text: "Settings",
                 icon: Icon(
                   Icons.settings,
-                  color: selected[4] ? Colors.green : Colors.grey,
+                  color: selected[3] ? Colors.green : Colors.grey,
                 ),
               )
             ],
